a3014b6ccda7b0c3a2b5c258eb246eefda7812f6
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-03-16 09:51:23 +0000

    Addition of two number

---
+++ .devcontainer/Main folder/Addition.java
@@ -0,0 +1,16 @@
+public class Addition {
+
+    public static void main(String[] args) {
+        System.out.print("Addition of two Number : ");
+        Addition ob1 = new Addition();
+        int x = 45;
+        int y = 23;
+        int res = ob1.add(x,y);
+        System.out.println(res);
+
+    }
+
+    public int add(int a, int b) {
+        return a = b;
+    }
+}
