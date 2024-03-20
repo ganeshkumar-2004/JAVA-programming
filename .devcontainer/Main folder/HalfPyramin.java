6f4787acddebf448a060c01ecbe9ad465474ee74
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-03-20 09:16:31 +0000

    Half Left pyramin

--- .devcontainer/Main folder/HalfPyramin.java
+++ .devcontainer/Main folder/HalfPyramin.java
@@ -1,14 +1,16 @@
 import java.util.*;
+
 public class HalfPyramin {
 
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter the Number of rows: ");
-        int n=sc.nextInt();
-        for (int i = 1; i <=n; i++) {
-            for (int j = 1; j <=n; j++) {
-                if(j>=n+1-i && j<=n)
-                     System.out.print("*");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j >= n + 1 - i && j <= n)
+                    System.out.print("*");
                 else
                     System.out.print(" ");
             }
