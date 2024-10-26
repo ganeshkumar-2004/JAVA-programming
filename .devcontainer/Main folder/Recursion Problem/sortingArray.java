d75ebe2b49952ca64ff6c479ff9f14afceb6913c
Ganesh Kumar <153806972+ganeshkumar-2004@users.noreply.github.com>
2024-10-26 15:57:24 +0000

    Sorting a Vector by using recursion

--- .devcontainer/Main folder/Recursion Problem/sortingArray.java
+++ .devcontainer/Main folder/Recursion Problem/sortingArray.java
@@ -6,7 +6,7 @@ import java.util.Vector;
  */
 public class sortingArray {
 
-    public void sort(Vector<Integer> vec) {
+    public static void sort(Vector<Integer> vec) {
         if (vec.size() == 0) {
             return;
         } else {
@@ -18,33 +18,33 @@ public class sortingArray {
 
     }
 
-    void insertelement(Vector<Integer> vec, int temp) {
+    public static void insertelement(Vector<Integer> vec, int temp) {
         if (vec.size() == 0 || vec.get(vec.size() - 1) <= temp) {
             vec.add(temp);
         } else {
             int val = vec.get(vec.size() - 1);
-            vec.remove(vec.size()-1);
+            vec.remove(vec.size() - 1);
             insertelement(vec, temp);
             vec.add(val);
         }
     }
 
     public static void main(String[] str) {
-        sortingArray arr = new sortingArray();
+
         Vector<Integer> v = new Vector<>();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of the element of the Vector : ");
         int n = sc.nextInt();
-        System.out.print("Enter the element of the Vector : " );
-      for (int i = 0; i < n; i++) {
-        int element = sc.nextInt();
-        v.add(element);
+        System.out.print("Enter the element of the Vector : ");
+        for (int i = 0; i < n; i++) {
+            int element = sc.nextInt();
+            v.add(element);
 
-      }
-      arr.sort(v);
-      System.out.print("The sorted vector is : ");
-      for (Integer i : v) {
-        System.out.print(i +" ");
-       }
+        }
+        sortingArray.sort(v);
+        System.out.print("The sorted vector is : ");
+        for (Integer i : v) {
+            System.out.print(i + " ");
+        }
     }
 }
